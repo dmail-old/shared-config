f5a859dd72e0bcd40aeab53f50afd5dcfa695ca0
Damien Maillard <dmaillard06@gmail.com>
2017-09-16 13:49:32 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const deepMerge = require("deepmerge")
+const eslintConfig = require("./src/eslint")
+const prettierConfig = require("./src/prettier")
+
+const namedConfig = {
+	eslint: eslintConfig,
+	prettierConfig: prettierConfig
+}
+
+const config = (name, ...overrides) =>
+	deepMerge.all([namedConfig[name], ...overrides], { clone: true })
+exports.config = config
---
+++ src/prettier/prettier.js
@@ -0,0 +1,7 @@
+const prettierConfig = {
+	printWidth: 100,
+	semi: false,
+	tabWidth: 2,
+	useTabs: true
+}
+exports.config = prettierConfig
