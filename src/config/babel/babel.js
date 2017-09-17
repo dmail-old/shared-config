307af7613a5ae498062388f2cdc5072fe68743c3
Damien Maillard <dmaillard06@gmail.com>
2017-09-17 14:28:14 +0200

    transpile any index.test.js too

--- src/config/babel/babel.js
+++ src/config/babel/babel.js
@@ -2,6 +2,6 @@ const babelConfig = {
 	presets: ["env"],
 	plugins: [],
 	ignore: ["node_modules", "dist"],
-	only: ["index.js", "src/*"]
+	only: ["index.js", "index.test.js", "src/*"]
 }
 exports.config = babelConfig
