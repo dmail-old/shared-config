47ed6cfc6f1eccbf9bf2bffc4f31d7f03245e13f
Damien Maillard <damien.maillard@dailymotion.com>
2017-12-11 12:28:51 +0100

    [prettier] add arrowParens always

--- src/config/prettier/prettier.js
+++ src/config/prettier/prettier.js
@@ -4,5 +4,11 @@ const prettierConfig = {
 	tabWidth: 2,
 	useTabs: true,
 	trailingComma: "all",
+	// arrow function with a single parameter is conceptually great
+	// BUT
+	// param destructuring requires parenthesis
+	// when adding new parameter it becomes a syntax error you have to manually
+	// add parenthesis back, which is painful
+	arrowParens: "always",
 }
 exports.config = prettierConfig
